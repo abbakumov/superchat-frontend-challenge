800e9bc43fddcfab804b418cc6a5e2089384a275
Евгений <eugene@192.168.1.108>
2022-03-14 21:13:34 +0300

    Home page view w/o real data component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,32 @@
 import type { NextPage } from 'next';
+import SearchInput from '../components/search-input';
+import IconSelector from '../components/icon-selector';
+import ColorSelector from '../components/color-selector';
+import SubmitButton from '../components/submit-button';
 
 const Home: NextPage = () => (
-    <div />
+    <div className="min-h-screen p-10 bg-gray-100">
+        <div className="max-w-md mx-auto">
+            <h1 className="text-2xl text-center font-bold pb-8">Welcome to GitHub Page Generator!</h1>
+            <SearchInput
+                id="user-name"
+                labelText="Select User:"
+                value=""
+                onValueChange={(value) => { console.log('value: ', value); }}
+                cleanInput={() => { console.log('clean'); }}
+            />
+            <SearchInput
+                id="repo-name"
+                labelText="Select Repo:"
+                value="hey"
+                onValueChange={(value) => { console.log('value: ', value); }}
+                cleanInput={() => { console.log('clean'); }}
+            />
+            <IconSelector chosenIconCode={1} />
+            <ColorSelector chosenColorCode={2} />
+            <SubmitButton>Make Page!</SubmitButton>
+        </div>
+    </div>
 );
 
 export default Home;
